9c85bc911f0f2c02482f5c9e2f62ce24247d7c1e
AlejandroFuertes <ale.f_96@outlook.com.ar>
2020-04-12 16:32:07 -0300

    Ultimos cambios de la unidad 1

--- funcionesCadenas.h
+++ funcionesCadenas.h
@@ -17,4 +17,31 @@ void mostrarCadena(char *cadena){
     printf("%s",cadena);
 }
 
+/*FUNCION PARA IGNORAR LAS LETRAS Y QUEDARSE CON LOS NUMEROS QUE ESTEN EN FORMATO CARACTER*/
+int caracterValido(const char caracter){
+    return caracter >= '0' && caracter <= '9';
+}
+
+/*TRANSFORMA NUMEROS DE CARACTERES A ENTEROS*/
+int mi_Atoi(const char *cadena, int tamanio){
+
+    //VARIABLE PARA RECORRER EL VECTOR DE FIN A INICIO
+    int i = tamanio - 1;
+    //VARIABLE PARA UNIDAD, DECENA, CENTENA, ETC
+    int pos = 1;
+    //VARIABLE PARA IR ARMANDO EL NUMERO DE CHAR A INT
+    int armandoNro = 0;
+    while(i >= 0){
+
+        while(caracterValido(*(cadena + i))){
+
+            armandoNro = armandoNro + (*(cadena + i) - 48) * pos;
+            pos *= 10;
+            i--;
+        }
+        i--;
+    }
+    return armandoNro;
+}
+
 #endif // FUNCIONESCADENAS_H_INCLUDED
